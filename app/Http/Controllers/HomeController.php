48d085dae5e6d2304d27fc13d195fd73a9ef2f9a
théoo <90846584+theokbokki@users.noreply.github.com>
2025-02-23 07:02:26 +0100

    Cleanup base laravel install

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request): View
+    {
+        return view('home');
+    }
+}

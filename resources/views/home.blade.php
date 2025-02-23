9701699090c42b23e9bd5648940d98b6678eff44
théoo <90846584+theokbokki@users.noreply.github.com>
2025-02-23 07:11:18 +0100

    Add layout component

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,2 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Theoo.dev</title>
-
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="" id="app"></body>
-</html>
+<x-layout title="Theoo.dev">
+</x-layout>

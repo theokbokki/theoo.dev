cd2caf5a9605dd5d41acf2589b26f7a37085a85e
théoo <90846584+theokbokki@users.noreply.github.com>
2025-03-31 08:37:13 +0200

    Add nav

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,18 @@
-import './bootstrap';
+import "./bootstrap";
+import Nav from "./parts/Nav";
+
+class App {
+    constructor() {
+        this.setupPart(".nav", Nav);
+    }
+
+    setupPart(selector, className) {
+        document.querySelectorAll(selector).forEach((el) => {
+            new className(el);
+        });
+    }
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+    new App();
+});

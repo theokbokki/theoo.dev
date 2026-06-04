e8a6c7a3a78a99cf93d821f1ea751f77acb7c4b6
théoo <90846584+theokbokki@users.noreply.github.com>
2026-06-04 16:49:11 +0200

    Add content sections to page

--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -1,3 +1,11 @@
 <x-base>
     <x-crumbs :$page/>
+    <div>{!! str()->markdown($page->content) !!}</div>
+    <div>
+        @foreach($page->children as $child)
+            <div>
+                <a href="{{ route('page', ['page' => $child]) }}">{{ $child->title }}</a>
+            </div>
+        @endforeach
+    </div>
 </x-base>

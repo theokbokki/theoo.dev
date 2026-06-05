a02cf3ea37181a789f5e833d7e1e54879b7673e1
théoo <90846584+theokbokki@users.noreply.github.com>
2026-06-05 11:23:26 +0200

    Add crumbs style

--- resources/views/components/crumbs.blade.php
+++ resources/views/components/crumbs.blade.php
@@ -1,7 +1,7 @@
-<nav>
-    <h1>{{ $page->title }}</h1>
+<nav class="crumbs">
+    <h1 class="crumbs__crumb">{{ $page->title }}</h1>
 
     @foreach($crumbs as $page)
-        <a href="{{ route('page', ['page' => $page]) }}">{{ $page->title }}</a>
+        <a href="{{ route('page', ['page' => $page]) }}" class="crumbs__crumb crumbs__crumb--link">{{ $page->title }}</a>
     @endforeach
 </nav>

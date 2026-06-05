e6b5ff7c22597c431387234752e08aa0e083d04c
théoo <90846584+theokbokki@users.noreply.github.com>
2026-06-05 16:23:27 +0200

    Update crumbs styling

--- resources/views/components/crumbs.blade.php
+++ resources/views/components/crumbs.blade.php
@@ -2,6 +2,8 @@
     <h1 class="crumbs__crumb">{{ $page->title }}</h1>
 
     @foreach($crumbs as $page)
-        <a href="{{ route('page.show', ['page' => $page]) }}" class="crumbs__crumb crumbs__crumb--link">{{ $page->title }}</a>
+        <a href="{{ route('page.show', ['page' => $page]) }}" class="crumbs__crumb">
+            <span class="crumbs__text">{{ $page->title }}</span>
+        </a>
     @endforeach
 </nav>

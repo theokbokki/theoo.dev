b318245b71207bc05c248d0c9eecce06f6fa56e6
théoo <90846584+theokbokki@users.noreply.github.com>
2026-06-05 10:23:00 +0200

    Add auth only markup

--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -1,10 +1,33 @@
 <x-base>
     <x-crumbs :$page/>
-    <div>{!! str()->markdown($page->content) !!}</div>
+    @auth()
+        <textarea name="editor" id="editor">{!! $page->content !!}</textarea>
+    @else
+        <div>{!! str()->markdown($page->content) !!}</div>
+    @endauth
     <div>
         @foreach($page->children as $child)
             <div>
                 <a href="{{ route('page', ['page' => $child]) }}">{{ $child->title }}</a>
+                @auth
+                    <div>
+                        <button type="button">
+                            <span class="sro">
+                                @if($child->pinned) Pin @else Unpin @endif
+                            </span>
+                        </button>
+                        <button type="button">
+                            <span class="sro">
+                                @if($child->draft) Undraft @else Draft @endif
+                            </span>
+                        </button>
+                        <button type="button">
+                            <span class="sro">
+                                @if($child->private) Public @else Private @endif
+                            </span>
+                        </button>
+                    </div>
+                @endauth
             </div>
         @endforeach
     </div>

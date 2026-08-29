9ddb9404c50755c71b0252569411d6473ba58ac3
théoo <90846584+theokbokki@users.noreply.github.com>
2026-08-29 11:41:51 +0200

    Add actions

--- modules/notes/routes/notes-routes.php
+++ modules/notes/routes/notes-routes.php
@@ -6,3 +6,5 @@ use Modules\Notes\Http\Controllers\NotesIndexController;
 Route::get('notes', NotesIndexController::class)->name('notes.index');
 
 Route::get('notes/{slug}', function () {})->name('notes.single');
+
+Route::post('notes/create', function () {})->name('notes.create');

f31273c40d1b124b3a0dae4788ea15fec0720a68
théoo <90846584+theokbokki@users.noreply.github.com>
2026-09-14 13:05:55 +0200

    Update note edit

--- resources/views/notes/edit.blade.php
+++ resources/views/notes/edit.blade.php
@@ -1,20 +1,26 @@
-<x-layout baseClass="notes">
-    <header class="header">
-        <h1 class="header__title">Edit note "{{ $note->title }}"</h1>
-        <a href="{{ route('notes.show', ['note' => $note]) }}" class="header__back">← Back to note</a>
-        <x-nav/>
-    </header>
-    <form class="notes__form notes__form--edit">
-        @csrf
-        <div class="actions">
-            <button type="submit" formaction="{{ route('notes.update', ['note' => $note]) }}" formmethod="POST" class="actions__action">Save</button>
-            <div>
-                <label for="upload-image" class="actions__action">Add image</button>
-                <input type="file" name="upload-image" id="upload-image" accept="image/*" hidden multiple/>
-            </div>
-            <button type="submit" formaction="{{ route('notes.delete', ['note' => $note]) }}" formmethod="POST" class="actions__action actions__action--danger">Delete</button>
+<x-layout baseClass="notes-form">
+    <x-form-header title="New note" :href="route('notes.index')">
+        <x-slot:actions>
+            <x-button type="submit" formaction="{{ route('notes.update', ['note' => $note]) }}" formmethod="POST" form="note-edit-form">Edit</x-button>
+        </x-slot>
+    </x-form-header>
+    <div class="notes-form__tools">
+        <div class="notes-form__container">
+            <x-button type="button" data-action="image" icon="image" modifiers="icon transparent">Upload image</x-button>
+            <x-button type="button" data-action="link" icon="link" modifiers="icon transparent">Add link</x-button>
+            <x-button type="button" data-action="quote" icon="quote" modifiers="icon transparent">Add quote</x-button>
+            <x-button type="button" data-action="bold" icon="bold" modifiers="icon transparent">Make bold</x-button>
+            <x-button type="button" data-action="italic" icon="italic" modifiers="icon transparent">Make italic</x-button>
         </div>
-        <textarea name="content" id="content" placeholder="Blablabla..." class="notes__edit">{!! old('content', $note->content) !!}</textarea>
-        @error('content') <p class="notes__error">{{ $message }}</p> @enderror
+    </div>
+    <form class="notes-form__form" id="note-edit-form">
+        @csrf
+        <textarea name="title" id="title" class="notes-form__title" placeholder="Note title...">{{ old('title', $note->title) }}</textarea>
+        @error('title') <p class="notes-form__error">{{ $message }}</p> @enderror
+
+        <textarea name="subtitle" id="subtitle" class="notes-form__subtitle" placeholder="Subtitle...">{{ old('subtitle', $note->subtitle) }}</textarea>
+
+        <textarea name="content" id="content" class="notes-form__content" placeholder="Blablabla...">{{ old('content', $note->content) }}</textarea>
+        @error('content') <p class="notes-form__error">{{ $message }}</p> @enderror
     </form>
 </x-layout>
